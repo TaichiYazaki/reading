4fe3b430b92be879aae0461d34e998b21ed8d9c7
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-04-18 11:11:31 +0900

    画像ファイルの表示

--- db/db.go
+++ db/db.go
@@ -29,10 +29,20 @@ func DbInit() {
 	db.AutoMigrate(&Reading{})
 }
 
-func DbCreate(title string, price string, review string, impression string) {
+func DbCreate(title string, price string, review string, file string, impression string) {
 	db, err := DatabaseConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.Create(&Reading{Title: title, Price: price, Review: review, Impression: impression})
+	db.Create(&Reading{Title: title, Price: price, Review: review, ImgFile: file, Impression: impression})
+}
+
+func DbFindAll() []Reading {
+	db, err := DatabaseConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var readings []Reading
+	db.Find(&readings)
+	return readings
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"os"
 	"reading/db"
 
 	"github.com/gin-gonic/gin"
@@ -11,19 +13,36 @@ func main() {
 	db.DbInit()
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	router.Static("/styles", "./styles")
+	router.Static("/img", "./img")
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
+		readings := db.DbFindAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"readings": readings,
+		})
 	})
 	router.GET("/create", func(ctx *gin.Context) {
 		ctx.HTML(200, "create.html", gin.H{})
+	})
+	router.GET("/item", func(ctx *gin.Context) {
+		ctx.HTML(200, "item.html", gin.H{
+			"ok": "success",
+		})
 	})
 	router.POST("/create", func(ctx *gin.Context) {
 		title := ctx.PostForm("title")
 		price := ctx.PostForm("price")
 		review := ctx.PostForm("review")
+		file := ctx.PostForm("file")
 		impression := ctx.PostForm("impression")
-		db.DbCreate(title, price, review, impression)
+		db.DbCreate(title, price, review, file, impression)
+		ctx.Redirect(302, "/")
+		filePath := "/Users/YAZAKITAICHI/env/go_env/reading/img/" + file
+		_, err := os.Create(filePath)
+		if err != nil {
+			log.Fatal("ファイル確保の失敗")
+		}
 		ctx.Redirect(302, "/")
 	})
 	router.Run(":8080")
